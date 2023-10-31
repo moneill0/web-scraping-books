b3038ba1885e3882856a6fcc4cc829168e4fe402
Mairead O'Neill <moneill@ibm.com>
2023-10-31 14:17:54 +0000

    load dataset

--- generate-books.py
+++ generate-books.py
@@ -1,1 +1,10 @@
-# TODO: generate fake book data based on scraped data
+import pandas as pd
+import os
+from pathlib import Path
+
+# Import books dataset
+abs_path = Path(".").absolute()
+books = pd.read_csv(str(abs_path) + os.sep + "Books.csv")
+book_titles = books["Titles"]
+
+# TODO: generate synthetic book titles based on scraped data
